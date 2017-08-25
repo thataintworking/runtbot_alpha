71add76f16af40548e943806dbc5201ef8a4cf62
Ron Smith <ron.smith@thataintworking.com>
2017-08-24 22:34:15 -0500

    Added TestPins to make sure the Nano was working properly. It is. My code just sucks.

---
+++ Arduino/TestPins/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sbpath=$(cd `dirname $0`/.. && pwd)
+
+arduino \
+    --$1 \
+    --board arduino:avr:nano:cpu=atmega328 \
+    --port /dev/ttyUSB0 \
+    --pref sketchbook.path=$sbpath \
+    TestPins.cpp
